04dacae2d48b686e7391255ae935b013868c91a7
Kilian Naudin <knaudin@synapsy.fr>
2023-01-04 13:52:14 +0100

    css + ajout material

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,31 +4,46 @@ export function Home() {
   return (
     <>
       <div className="container">
-        <h1>Bienvenue sur le site de la communauté Star-Citizen ODS!</h1>
-        <p>
+        <h1 className="title">
+          Bienvenue sur le site de la communauté Star-Citizen ODS!
+        </h1>
+        <p className="subtitle">
           Notre site est dédié à la communauté de joueurs de Star-Citizen et à
           notre entreprise ODS, spécialisée dans le commerce intergalactique.
           Rejoignez-nous pour échanger sur le jeu, découvrir les dernières
           nouveautés et trouver de nouvelles opportunités de commerce!
         </p>
-        <img src="logo.png" alt="Logo ODS" />
-        <h2>Menu</h2>
-        <ul>
-          <li>
-            <a href="/">Accueil</a>
-          </li>
-          <li>
-            <a href="/forum">Forum</a>
-          </li>
-          <li>
-            <a href="/commerce">Commerce</a>
-          </li>
-          <li>
-            <a href="/ods">ODS</a>
-          </li>
-        </ul>
-        <h2>Suivez-nous</h2>
-
+        <img src="logo.png" alt="Logo ODS" className="image is-128x128" />
+        <h2 className="subtitle">Menu</h2>
+        <div className="tabs is-centered">
+          <ul>
+            <li className="is-active">
+              <a>Accueil</a>
+            </li>
+            <li>
+              <a href="/forum">Forum</a>
+            </li>
+            <li>
+              <a href="/commerce">Commerce</a>
+            </li>
+            <li>
+              <a href="/ods">ODS</a>
+            </li>
+          </ul>
+        </div>
+        <h2 className="subtitle">Suivez-nous</h2>
+        <p>Rejoignez notre communauté sur les réseaux sociaux:</p>
+        <div className="buttons">
+          <a
+            className="button is-primary"
+            href="https://twitter.com/ods_starcitizen"
+          >
+            <span className="icon">
+              <i className="fab fa-twitter"></i>
+            </span>
+            <span>Twitter</span>
+          </a>
+        </div>
       </div>
     </>
   );
